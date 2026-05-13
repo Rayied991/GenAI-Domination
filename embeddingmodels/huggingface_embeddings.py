7f2f5e57989bd7dfb83e37c6e2077c5c3eaa9167
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-05-13 23:50:42 +0600

    Embedded models intro

---
+++ embeddingmodels/huggingface_embeddings.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding=HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+) 
+texts=[
+        "Hello this is Akarsh Vyas",
+        "Hello your name is Youtube",
+        "And you all are very beautiful"
+]
+
+vector=embedding.embed_documents(texts)
+print(vector)
