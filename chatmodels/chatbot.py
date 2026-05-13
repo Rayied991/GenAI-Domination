c13c339d143e9295de24b4f1bc8ebd6cb8d624ab
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-05-14 01:19:25 +0600

    UI of a chat bot and basic chatbot implemented

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -2,9 +2,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import AIMessage,SystemMessage,HumanMessage
+model=ChatMistralAI(model= "mistral-small-latest",temperature=0.9)
 
-model=ChatMistralAI(model= "mistral-small-2506",temperature=0.9)
-
-res= model.invoke("Write a poem on AI")
-
-print(res.content)
+messages=[
+    SystemMessage(content="You are a funny AI agent")
+    
+]
+print("------------ type 0 to exit application -------")
+while True:
+    prompt=input("You : ")
+    messages.append(HumanMessage(content=prompt))
+    if prompt == "0":
+        break
+    res= model.invoke(messages)
+    messages.append(AIMessage(content=res.content))
+    print("Bot : ",res.content)
+    
+print(messages)    
